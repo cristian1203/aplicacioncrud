ef475006232106478de4267070321b34e0274a2c
cristian1203 <151034282+cristian1203@users.noreply.github.com>
2024-03-08 13:37:20 +0100

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,16 +1,17 @@
 <?php
+// Define los parámetros de conexión a la base de datos.
+// Los parámetros se toman del fichero de variables de entorno: .env
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
-define('DB_HOST', 'mariadb');
-define('DB_NAME', 'crud_db');
-define('DB_USER', 'crud_user');
-define('DB_PASSWORD', 'crud_password');
+// Abre una nueva conexión al servidor MySQL/MariaDB
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-
-
-$mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-if (mysqli_connect_errno()) {
-    printf('Falló la conexión: %s\n', mysqli_connect_error());
+// Verifica si hay errores de conexión
+if ($mysqli->connect_errno) {
+    printf('Falló la conexión: %s\n', $mysqli->connect_error);
     exit();
 }
-
 ?>
